43a25b70d11d81c5ce7f67efb5f0c277bfdff28e
llr2ll <raphaelsanseverino@gmail.com>
2022-07-27 01:20:47 -0300

    fix

--- db.sql
+++ db.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `films_list` (
+	`key` int NOT NULL AUTO_INCREMENT,
 	`id` varchar(45) NOT NULL,
 	`title` varchar(45) NOT NULL,
 	`image` varchar(100) NOT NULL,
@@ -6,7 +7,7 @@ CREATE TABLE `films_list` (
 	`description` varchar(1000) NOT NULL,
 	`director` varchar(45) NOT NULL,
 	`producer` varchar(85) NOT NULL,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`key`)
 ) ENGINE InnoDB,
   CHARSET utf8mb4,
   COLLATE utf8mb4_unicode_ci;
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const app = express();
 const pool = require('./db.js')
+const port = 3000
 
-    app.listen(3000,() => {console.log('Sever rodando: http://localhost:3000')});
+    app.listen(port,() => {console.log(`Sever rodando: http://localhost:${port}`)});
     app.use(express.json())
-    app.use(express.urlencoded({ extended: true}));
+    app.use(express.urlencoded({extended: true}));
     app.use((req,res,next) =>{
-      res.header('Access-Control-Allow-Origin','*')
-      res.header('Access-Control-Allow-Header','Origin, X-Requrested-With, Content-Type, Accept, Authorization')
-      if(req.method === 'OPTIONS'){
-        res.header('Acess-Control-Allow-Methods','GET,POST,DELETE')
-        return res.status(200)
-      }
-      next();
+        res.header('Access-Control-Allow-Origin','*')
+        res.header('Access-Control-Allow-Header','Origin, X-Requrested-With, Content-Type, Accept, Authorization')
+        if(req.method === 'OPTIONS'){
+          res.header('Acess-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET')
+          return res.status(200)
+        }
+        next();
     })
 
     app.post('/', async(req,res) =>{
         const {id, title, image, movie_banner, description, director, producer} = req.body
-        
+
         try {   
           const [result] = await pool.query('INSERT INTO films_list SET ?', {id, title, image, movie_banner, description, director, producer});
           return res.status(200).json(result);
@@ -31,12 +32,12 @@ const pool = require('./db.js')
           return res.status(200).json(result);
         } 
         catch (error) { return res.status(500).json({ message: error.message }) }
-    });
+    })
 
     app.delete('/films/:id', async(req, res) => {
-            try {
-              const [result] = await pool.query('Delete FROM films_list WHERE id=?',(req.params.id));
-              return res.status(200).json(result);
-            } 
-            catch (error) { return res.status(500).json({ message: error.message }) }
-        })
+        try {
+          const [result] = await pool.query('Delete FROM films_list WHERE id=?',(req.params.id));
+          return res.status(200).json(result);
+        } 
+        catch (error) { return res.status(500).json({ message: error.message }) }
+    })
